93919c3841a8be614f3bc634c481b2610469ff71
Yoel Raga <yoelraga81@gmail.com>
2022-12-19 11:44:56 +0700

    Desain button

--- app/src/main/java/com/example/fileexploler/MainActivity.kt
+++ app/src/main/java/com/example/fileexploler/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.fileexploler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MaterialButton storageBtn = findViewById(R.id.storage_btn);
+
+
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(activity: Main)
     }
 }
